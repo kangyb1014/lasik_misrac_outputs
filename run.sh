d1d846087ed0818b3d7389369dc318915afc8ab9
Yeongbin Kang <clear1014@gmail.com>
2025-08-12 12:19:15 +0000

    update

--- run.sh
+++ run.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname "$0")
-model_name="gpt-4.1-nano"
-
-
 
 python3 lasik/main.py \
-    -o /ssd_home/yeongbin/lasik/output/$(date +%Y%m%d.%H%M)_gpt-4.1-mini-libmicrohttpd \
-    -m gpt-4.1-mini \
-    -p libmicrohttpd \
-    -v 9 \
-    -l /ssd_home/yeongbin/lasik/output/libmicrohttpd_sample.json
-
-python3 lasik/main.py \
-    -o /ssd_home/yeongbin/lasik/output/$(date +%Y%m%d.%H%M)_gpt-5-mini-libmicrohttpd \
+    -o /ssd_home/yeongbin/lasik/output/$(date +%Y%m%d.%H%M)_gpt-5-mini-osal \
     -m gpt-5-mini \
-    -p libmicrohttpd \
+    -p osal \
     -v 9 \
-    -l /ssd_home/yeongbin/lasik/output/libmicrohttpd_sample.json
+    -l /ssd_home/yeongbin/lasik/output/osal_sample.json
 
 python3 lasik/main.py \
     -o /ssd_home/yeongbin/lasik/output/$(date +%Y%m%d.%H%M)_gpt-5-mini-littlefs \
@@ -41,6 +31,6 @@ python3 lasik/main.py \
     -F /ssd_home/yeongbin/lasik/misrac_subjects/libmicrohttpd/srcs/libmicrohttpd-1.0.1/src/microhttpd/mhd_threads.c \
     -f MHD_create_named_thread_
 
-python3 convert_output_to_data.py /ssd_home/yeongbin/lasik/output/20250808.08\:12_gpt-5-mini-libmicrohttpd/ /dev/null /ssd_home/yeongbin/lasik/output/20250808.08\:12_gpt-5-mini-libmicrohttpd/libmicrohttpd.json
+# python3 convert_output_to_data.py /ssd_home/yeongbin/lasik/output/20250808.08\:12_gpt-5-mini-libmicrohttpd/ /dev/null /ssd_home/yeongbin/lasik/output/20250808.08\:12_gpt-5-mini-libmicrohttpd/libmicrohttpd.json
 
-python3 execution_summary.py ../../output/20250808.08\:12_gpt-5-mini-libmicrohttpd/analysis_results/ > ../../output/20250808.08\:12_gpt-5-mini-libmicrohttpd/execution_summary.csv
+# python3 execution_summary.py ../../output/20250808.08\:12_gpt-5-mini-libmicrohttpd/analysis_results/ > ../../output/20250808.08\:12_gpt-5-mini-libmicrohttpd/execution_summary.csv
